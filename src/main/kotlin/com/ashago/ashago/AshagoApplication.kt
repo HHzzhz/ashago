5df36b376215d19f48d59136d7fd39e99b37fb03
huazheng <huazheng@lattebank.com>
2020-02-19 19:14:53 +0800

    init

---
+++ src/main/kotlin/com/ashago/ashago/AshagoApplication.kt
@@ -0,0 +1,11 @@
+package com.ashago.ashago
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AshagoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AshagoApplication>(*args)
+}
